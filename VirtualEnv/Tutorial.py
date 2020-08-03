a395348a93d9719762ac872b60726ac4a2eefb5e
Kerwin Zhou <DerekZhou2015@outlook.com>
2020-08-03 20:38:06 +0800

    VitualEnv Finished

---
+++ VirtualEnv/Tutorial.py
@@ -0,0 +1,7 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+'a test class'
+__author__ = 'Kerwin'
+import webbrowser
+webbrowser.open_new('https://www.cnblogs.com/cwp-bg/p/python.html')
+print("----------------------------------")
